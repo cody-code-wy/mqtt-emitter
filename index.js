998fcd62a4bda4749c0e863c4572420c8452292b
William Young <william@werl.me>
2021-11-23 16:20:42 -0600

    update

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ var client = mqtt.connect('mqtt://rabbitmq-service:1883',{clientId:'mqttjs01'});
 client.on("connect", (c) => {
   console.log('connected')
   console.log(JSON.stringify(c))
-  if ( c.clean ) {
-    console.log("Sending testtopic");
-    client.publish("testtopic", "test message")
-  }
 });
 client.on("error", (e) => { console.log(`Can't connect ${e}`) });
 client.on("reconnect", (e) => { console.log("reconnecting") });
 client.on("close", (e) => { console.log("closed") });
 
+setInterval( () => {
+  if ( client.connected == true ) {
+    console.log("Sending root");
+    client.publish("openflowx/root", JSON.stringify({key: 'value'}));
+  }
+}, 1000 );
